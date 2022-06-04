d6bbd722cc4b58d55ef668d6d58980f3f1b94090
zzfima <zzfima@gmail.com>
2022-06-04 20:01:30 +0300

    fib ver 1

---
+++ Fib/Fib/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonachi1(11));
+        }
+
+        public static int Fibonachi1(int steps)
+        {
+            int current = 1;
+            int previous = 0;
+
+            if (steps == 0)
+                return 0;
+            else if (steps == 1)
+                return 1;
+
+            for (int i = 2; i < steps; i++)
+            {
+                var tmp = current;
+                current += previous;
+                previous = tmp;
+            }
+
+            return current;
+        }
+    }
+}
